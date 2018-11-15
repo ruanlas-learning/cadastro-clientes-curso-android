36ca87993f9ccde5f9e55296344a372753c688ec
Ruan Silva <ruanluiz.as@gmail.com>
2018-11-15 10:23:37 -0200

    aula 14

--- app/src/main/java/com/example/ruan/cadastrodeclientes/ActCadClientes.java
+++ app/src/main/java/com/example/ruan/cadastrodeclientes/ActCadClientes.java
@@ -6,8 +6,14 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class ActCadClientes extends AppCompatActivity {
+
+    private EditText edtNome, edtEmail, edtTelefone, edtEndereco, edtObservacoes;
+    private Spinner spnTelefone, spnEmail, spnEndereco;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +21,16 @@ public class ActCadClientes extends AppCompatActivity {
         setContentView(R.layout.act_cad_clientes);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        edtEmail = (EditText)findViewById(R.id.edt_email);
+        edtNome = (EditText)findViewById(R.id.edt_nome);
+        edtTelefone = (EditText)findViewById(R.id.edt_telefone);
+        edtEndereco = (EditText)findViewById(R.id.edt_endereco);
+        edtObservacoes = (EditText)findViewById(R.id.edt_observacoes);
+
+        spnEmail = (Spinner)findViewById(R.id.spn_email);
+        spnEndereco = (Spinner)findViewById(R.id.spn_endereco);
+        spnTelefone = (Spinner)findViewById(R.id.spn_telefone);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
